f2f1840c1f0ac4ae040a26003d6abe1d688bb874
Juan Dematei <juandematei@gmail.com>
2019-08-28 22:31:32 -0300

    json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $.ajax({
     type: "GET",
-    dataType: "jsonp",
+    dataType: "json",
     url: "https://apitransporte.buenosaires.gob.ar/ecobici/gbfs/stationStatus?client_id=c9f17951eca1433a8744072cd6ed90c9&client_secret=d16cdAd7C5a44875825649808f94ca6B",
     success: function(data) {
       console.log(data);
