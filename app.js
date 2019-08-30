7d9e05682673e59b10f41b5895af1fb3bbaa9efb
Juan Dematei <juandematei@gmail.com>
2019-08-30 10:06:57 -0300

    first version

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 (function() {
-  const url = "https://apitransporte.buenosaires.gob.ar/ecobici/gbfs/stationStatus";
-  const client_id = "c9f17951eca1433a8744072cd6ed90c9";
-  const client_secret = "d16cdAd7C5a44875825649808f94ca6B";
+  //const url = "https://apitransporte.buenosaires.gob.ar/ecobici/gbfs/stationStatus?";
+  //const client_id = "c9f17951eca1433a8744072cd6ed90c9";
+  //const client_secret = "d16cdAd7C5a44875825649808f94ca6B";
 
   $(document).ready(function() {
     $.ajax({
       type: "GET",
       dataType: "json",
       url: "https://apitransporte.buenosaires.gob.ar/ecobici/gbfs/stationStatus?client_id=c9f17951eca1433a8744072cd6ed90c9&client_secret=d16cdAd7C5a44875825649808f94ca6B",
+
       success: function(data) {
         var response = data.data.stations;
-        //console.log(response);
+        console.log(response);
 
         var totalDisabled = 0;
 
@@ -18,14 +19,17 @@
           var stationID = response[i].station_id;
           var stationNumBikesDisabled = response[i].num_bikes_disabled;
 
-          console.log("Estación " + stationID + ": " + stationNumBikesDisabled + " bicis bloqueadas.");
+          //console.log("Estación " + stationID + ": " + stationNumBikesDisabled + " bicis bloqueadas.");
 
           totalDisabled = totalDisabled + response[i].num_bikes_disabled;
         }
 
-        console.log("Hay " + totalDisabled + " bicis bloquedas en la red.");
+        //console.log("Hay " + totalDisabled + " bicis bloquedas en la red.");
 
         $(".response").append("Hay " + totalDisabled + " bicis bloquedas en la red.");
+      },
+      error: function(data) {
+        $(".response").append("ERROR");
       }
     });
   });
