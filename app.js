9a92e29f2cf44c517e32960d97f14753feb7951f
Juan Dematei <juandematei@gmail.com>
2019-09-04 16:43:39 -0300

    first version

--- app.js
+++ app.js
@@ -21,17 +21,21 @@
         //console.log(response);
 
         var totalDisabled = 0;
+        //var totalAvailable = 0;
 
         for (var i = 0; i < response.length; i++) {
           var stationID = response[i].station_id;
           var stationNumBikesDisabled = response[i].num_bikes_disabled;
+          var stationNumBikesAvailable = response[i].num_bikes_available;
 
-          //console.log("Estación " + stationID + ": " + stationNumBikesDisabled + " bicis bloqueadas.");
+          console.log("Estación " + stationID + ": " + stationNumBikesDisabled + " bicis bloqueadas.");
 
           totalDisabled = totalDisabled + response[i].num_bikes_disabled;
+          totalAvailable = totalAvailable + response[i].num_bikes_available;
         }
 
-        //console.log("Hay " + totalDisabled + " EcoBici bloqueadas");
+        console.log("Hay " + totalDisabled + " EcoBici bloqueadas");
+        console.log("Hay " + totalAvailable + " EcoBici disponibles");
 
         $(".response").append("Hay " + totalDisabled + " EcoBici bloqueadas");
         $(".updating").hide();
