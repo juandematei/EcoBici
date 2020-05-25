92dff171808f55ae3ea7db44fc66b0237b7d0184
juandematei <juandematei@gmail.com>
2020-05-25 20:51:01 -0300

    new version

--- dist/index.js
+++ dist/index.js
@@ -26,8 +26,8 @@ let bikesFakeInitial = 0;
 //  Search -------------------------------------------------------------------->
 let stationsValid = [];
 let stationsLocation = [];
+let stationsActive = [];
 let searchValue = "";
-let searchFixed = false;
 //  Chart --------------------------------------------------------------------->
 let chartAvailable = "";
 let chartDisabled = "";
@@ -50,6 +50,8 @@ const navStatusBtn = document.querySelector("#navStatusBtn");
 const searchSection = document.querySelector("#searchSection");
 //  Updating ------------------------------------------------------------------>
 const updating = document.querySelector("#updating");
+const updatingCardTotals = document.querySelector("#cardTotals > #updating");
+const updatingcardNearest = document.querySelector("#cardNearest > #updating");
 //  Response ------------------------------------------------------------------>
 const updateTime = document.querySelector("#updateTime");
 //  Cards --------------------------------------------------------------------->
@@ -65,6 +67,10 @@ const cardNearestStationBikesDisabled = document.querySelector("#cardNearestStat
 const cardNearestStationDocksAvailable = document.querySelector("#cardNearestStationDocksAvailable");
 const cardNearestStationMap = document.querySelector("#cardNearestStationMap");
 const cardNearestStationMapLink = document.querySelector("#cardNearestStationMapLink");
+const cardLocationButton = document.querySelector("#cardLocationButton");
+//  Active stations
+const stationList = document.querySelector(".response-table");
+const cardActiveSubtitle = document.querySelector("#cardActiveSubtitle");
 
 //* Twitter button ------------------------------------------------------------>
 const cardBikesTwitterButton = document.querySelector("#cardBikesTwitterButton");
@@ -76,8 +82,9 @@ const related = "elbotonmalo,baecobici";
 
 //! Main functions ------------------------------------------------------------>
 // Get accumulated quantities ------------------------------------------------->
-function bikesTotal() {
-  updating.classList.remove("updating--hide");
+function getBikesTotal() {
+  updatingCardTotals.classList.remove("updating--hidden");
+  updatingcardNearest.classList.remove("updating--hidden");
 
   let xhr = new XMLHttpRequest();
   xhr.open("GET", xhrStatus, true);
@@ -123,11 +130,11 @@ function bikesTotal() {
       let responseDate = new Date(resp.last_updated * 1000);
       updateTime.textContent = responseDate.toLocaleString("es-AR", options);
 
-      updating.classList.add("updating--hide");
-      cardTotals.classList.remove("card--hidden");
+      updatingCardTotals.classList.add("updating--hidden");
+      //cardTotals.classList.remove("card--hidden");
 
       // !Debug
-      // console.log("Response stationStatus - bikesTotal: ");
+      // console.log("Response stationStatus - getBikesTotal: ");
       // console.log(stationStatus);
       // console.log(`bikes: ${bikesAvailableAcc} / ${bikesDisabledAcc}`);
       // console.log(`fake bikes: ${bikesFakeAcc}`);
@@ -143,8 +150,7 @@ function bikesTotal() {
 }
 
 // Get quantities by station (search) ----------------------------------------->
-function bikesStation(busqueda) {
-  //updating.classList.remove("updating--hide");
+function getBikesStation(busqueda) {
 
   let xhr = new XMLHttpRequest();
   xhr.open("GET", xhrInformation, true);
@@ -153,7 +159,7 @@ function bikesStation(busqueda) {
       // Success!
       let resp = JSON.parse(this.response);
       let stationInformation = resp.data.stations;
-      //console.log("Response stationInformation - bikesStation:");
+      //console.log("Response stationInformation - getBikesStation:");
       //console.log(stationInformation);
 
       // Find station_id ------------------------------------------------------>
@@ -178,7 +184,7 @@ function bikesStation(busqueda) {
           // Success!
           let resp = JSON.parse(this.response);
           let stationStatus = resp.data.stations;
-          //console.log("Response stationStatus - bikesStation:");
+          //console.log("Response stationStatus - getBikesStation:");
           //console.log(stationStatus);
 
           // Get station status for station_id -------------------------------->
@@ -234,8 +240,8 @@ function bikesStation(busqueda) {
           //   searchValue = ""; //! Important
           // }
 
-          //updating.classList.add("updating--hide");
-          cardNearest.classList.remove("card--hidden");
+          updatingcardNearest.classList.add("updating--hidden");
+          //cardNearest.classList.remove("card--hidden");
 
         } else {
           //updating.classList.add("updating--hide");
@@ -255,8 +261,8 @@ function bikesStation(busqueda) {
   xhr.send();
 }
 
-// Get valid station numbers & location --------------------------------------->
-function getStationsValid() {
+// Get stations information --------------------------------------------------->
+function getStationInformation() {
   let xhr = new XMLHttpRequest();
   xhr.open("GET", xhrInformation, true);
   xhr.onload = function () {
@@ -267,21 +273,23 @@ function getStationsValid() {
 
       for (let i = 0; i < stationInformation.length; i++) {
         // Get each station number from station name
-        stationNumber = stationInformation[i].name.slice(0, 3);
-        stationsValid.push(stationNumber);
+        //stationNumber = stationInformation[i].name.slice(0, 3);
+        stationNumber = stationInformation[i].name
+        stationName = stationNumber.toUpperCase();
+        stationsValid.push(stationName);
         // Get each station location (lat & lon)
         stationsLocation.id = stationInformation[i].name.slice(0, 3);
         stationsLocation.lat = stationInformation[i].lat;
         stationsLocation.lon = stationInformation[i].lon;
         stationsLocation.push([stationsLocation.id, stationsLocation.lat, stationsLocation.lon]);
       }
-      //console.log("stationsValid");
-      //console.log(stationsValid);
 
-      //console.log("stationsLocation");
-      //console.log(stationsLocation);
+      //! Debug
+      // console.log("stationsValid", stationsValid);
+      // console.log("stationsLocation", stationsLocation);
 
       checkGeolocation();
+
     } else {
       //updating.classList.add("updating--hide");
     }
@@ -315,6 +323,46 @@ function getUniqueStatus() {
   };
   xhr.onerror = function () {
     //updating.classList.add("updating--hide");
+  };
+  xhr.send();
+}
+
+// Get active station numbers & location --------------------------------------->
+function getActiveStations() {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", xhrInformation, true);
+  xhr.onload = function () {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      let resp = JSON.parse(this.response);
+      let stationInformation = resp.data.stations;
+
+      for (let i = 0; i < stationInformation.length; i++) {
+        stationsActive.id = stationInformation[i].name.slice(0, 3);
+        stationsActive.name = stationInformation[i].name;
+        stationsActive.lat = stationInformation[i].lat;
+        stationsActive.lon = stationInformation[i].lon;
+        stationsActive.push([stationsActive.id, stationsActive.name, stationsActive.lat, stationsActive.lon]);
+        var row = stationList.insertRow(i + 1);
+        var cell1 = row.insertCell(0);
+        cell1.innerHTML = stationsActive.id;
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML = stationsActive.name;
+        var cell3 = row.insertCell(2);
+        cell3.innerHTML = stationsActive.lat;
+        var cell4 = row.insertCell(3);
+        cell4.innerHTML = stationsActive.lon;
+      }
+      cardActiveSubtitle.textContent = stationsActive.length;
+      updating.classList.add("updating--hide");
+    } else {
+      responseHeader.textContent = "Error";
+      updating.classList.add("updating--hide");
+    }
+  };
+  xhr.onerror = function () {
+    responseHeader.textContent = "Error";
+    updating.classList.add("updating--hide");
   };
   xhr.send();
 }
@@ -369,7 +417,6 @@ function hideNavBar() {
   }
 }
 
-
 // Add leading zeros to station_id number ------------------------------------->
 function pad(n) {
   if (n <= 999) {
@@ -378,23 +425,39 @@ function pad(n) {
   return n;
 }
 
-//* Search by user location --------------------------------------------------->
-// Get User's Coordinate from their Browser
-function checkGeolocation() {
-  // Geolocation OK
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(UserLocation);
+// Export CSV
+// Quick and simple export target #table_id into a csv
+function download_table_as_csv(table_id) {
+  // Select rows from table_id
+  var rows = document.querySelectorAll("table#" + table_id + " tr");
+  // Construct csv
+  var csv = [];
+  for (var i = 0; i < rows.length; i++) {
+    var row = [],
+      cols = rows[i].querySelectorAll("td, th");
+    for (var j = 0; j < cols.length; j++) {
+      // Clean innertext to remove multiple spaces and jumpline (break csv)
+      var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, "").replace(/(\s\s)/gm, " ");
+      // Escape double-quote with double-double-quote (see https://stackoverflow.com/questions/17808511/properly-escape-a-double-quote-in-csv)
+      data = data.replace(/"/g, '""');
+      // Push escaped string
+      row.push('"' + data + '"');
+    }
+    csv.push(row.join(";"));
   }
-  // Geolocation not available
-  else {
-    console.log("No hay ubicacion");
-  }
+  var csv_string = csv.join("\n");
+  // Download it
+  var filename = "export_" + table_id + "_" + new Date().toLocaleDateString() + ".csv";
+  var link = document.createElement("a");
+  link.style.display = "none";
+  link.setAttribute("target", "_blank");
+  link.setAttribute("href", "data:text/csv;charset=utf-8," + encodeURIComponent(csv_string));
+  link.setAttribute("download", filename);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
 
-// Callback function for asynchronous call to HTML5 geolocation
-function UserLocation(position) {
-  nearestStation(position.coords.latitude, position.coords.longitude);
-}
 
 // Convert Degress to Radians
 function Deg2Rad(deg) {
@@ -413,6 +476,27 @@ function PythagorasEquirectangular(lat1, lon1, lat2, lon2) {
   return d;
 }
 
+
+//* Search by user location --------------------------------------------------->
+//  Get user location --------------------------------------------------------->
+function checkGeolocation() {
+  // Geolocation OK
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(userLocation);
+  }
+  // Geolocation not available
+  else {
+    console.log("No hay ubicacion");
+    nearestStation(-34.6038157, -58.3816604);
+  }
+}
+
+// Callback function for asynchronous call to HTML5 geolocation
+function userLocation(position) {
+  nearestStation(position.coords.latitude, position.coords.longitude);
+}
+
+// Find nearest station ------------------------------------------------------->
 function nearestStation(latitude, longitude) {
   let minDif = 99999;
   let closest;
@@ -425,28 +509,39 @@ function nearestStation(latitude, longitude) {
     }
   }
 
+  // Get information for nearest station (search) ----------------------------->
   searchLocation = stationsLocation[closest][0];
+  getBikesStation(searchLocation);
 
+  // Get google maps directions url ------------------------------------------->
   let origin = latitude + "," + longitude;
   let destination = stationsLocation[closest][1] + "," + stationsLocation[closest][2];
   let mapLink = `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}&travelmode=walking`;
+  cardNearestStationMapLink.href = mapLink;
+
+  // Get google maps static map image ----------------------------------------->
   let mapImg = `https://maps.googleapis.com/maps/api/staticmap?center=${destination}&zoom=15&size=300x150&scale=2&markers=color:0xe66300%7C${destination}&key=${maps_api_key}`;
   cardNearestStationMap.src = mapImg;
-  cardNearestStationMapLink.href = mapLink;
-  bikesStation(searchLocation);
 }
 
 //* Event listeners ----------------------------------------------------------->
-navSearchBtn.addEventListener("click", function (event) {
+navSearchBtn.addEventListener("click", function (e) {
+  e.preventDefault();
   searchSection.classList.remove("search--hidden");
 });
-searchSection.addEventListener("click", function (event) {
+searchSection.addEventListener("click", function (e) {
+  e.preventDefault();
   searchSection.classList.add("search--hidden");
 });
+// cardLocationButton.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   console.log("click");
+// });
 
 
 //! Call functions on page load ----------------------------------------------->
 (function () {
-  getStationsValid();
-  bikesTotal();
+  getBikesTotal();
+  getStationInformation();
+  getActiveStations();
 })();
